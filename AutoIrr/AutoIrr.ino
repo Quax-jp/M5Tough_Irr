b612a7e208e40d59f395d91d1ac8fc4bab7df000
iwata <t-iwata@nippo-co.com>
2025-11-21 16:48:13 +0900

    図形を１つ追加

--- AutoIrr/AutoIrr.ino
+++ AutoIrr/AutoIrr.ino
@@ -17,6 +17,8 @@ the program in the setUp () function will be run, and this part will only be run
 M5Toughが起動またはリセットされると、setup()関数のプログラムの実行が開始されます。 */
 void setup() {
   M5.begin(); //Init M5Tough.  初期化 M5Tough
+
+  #if 0
   M5.Lcd.fillScreen(WHITE); // Set the screen background.  画面の背景色を白に設定する
   delay(500); //Delay 500ms.  遅延500ms
   M5.Lcd.fillScreen(RED);
@@ -46,8 +48,18 @@ void setup() {
   M5.Lcd.drawTriangle(30, 30, 180, 100, 80, 150, YELLOW); //Make a triangle wireframe with (x1,y1) (x2,y2) (x3,y3) as the vertices
   delay(1000);                                           //(x1,y1) (x2,y2) (x3,y3) を頂点とする三角形のワイヤーフレームを作る。
   M5.Lcd.fillTriangle(30, 30, 180, 100, 80, 150, YELLOW);   //(x1,y1) (x2,y2) (x3,y3) を頂点とする三角形を作る。
+  #endif
 
   M5.Lcd.clear(BLACK);
+
+  M5.Lcd.fillRect(0, 0, M5.Lcd.width(), 30, WHITE);//Draw a blue rectangle 50x50 at (x,y)
+
+  M5.Lcd.setTextSize(4);
+  M5.Lcd.setCursor(M5.Lcd.width() / 2, M5.Lcd.height() / 2);
+  M5.Lcd.printf("Center");
+
+  M5.Lcd.drawString("font_def", M5.Lcd.width() / 3,M5.Lcd.height() / 3);
+  M5.Lcd.drawCentreString("font_4", M5.Lcd.width() / 3 * 2,M5.Lcd.height() / 3 * 2, 4);      // drawCentreStringはフォント番号を省略するとエラーになる（例: 4 = 標準?, 7:7seg)
 }
 
 /* After the program in setup() runs, it runs the program in loop()
@@ -55,13 +67,7 @@ The loop() function is an infinite loop in which the program runs repeatedly
 setup()関数のプログラムが実行された後、loop()関数のプログラムが実行される。
 loop()関数は、プログラムを何度も繰り返すデッド・ループである。 */
 void loop(){
-  //M5.Lcd.clear();
-  M5.Lcd.setTextSize(4);
-  M5.Lcd.setCursor(M5.Lcd.width() / 2, M5.Lcd.height() / 2);
-  M5.Lcd.printf("Center");
-
-  M5.Lcd.drawString("font_def", M5.Lcd.width() / 3,M5.Lcd.height() / 3);
-  M5.Lcd.drawCentreString("font_4", M5.Lcd.width() / 3 * 2,M5.Lcd.height() / 3 * 2, 4);      // drawCentreStringはフォント番号を省略するとエラーになる（例: 4 = 標準?, 7:7seg)
+  
 
 
   M5.update();  //Read the press state of the key.  キーA、B、Cの状態を読む。
